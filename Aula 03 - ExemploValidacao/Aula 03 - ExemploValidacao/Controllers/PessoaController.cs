d9fc8a64d6a35091368f11517595dd648c7f04e6
jhondonavan <jhondonavan@gmail.com>
2015-11-19 20:44:10 -0200

    Index da pagina 03 pronta

---
+++ Aula 03 - ExemploValidacao/Aula 03 - ExemploValidacao/Controllers/PessoaController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aula_03___ExemploValidacao.Models;
+
+namespace Aula_03___ExemploValidacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+    }
+}
---
+++ Aula 03 - ExemploValidacao/Aula 03 - ExemploValidacao/Models/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Profile;
+
+namespace Aula_03___ExemploValidacao.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public string Observacao { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public string Senha { get; set; }
+
+        public string ConfirmarSenha { get; set; }
+    }
+}
