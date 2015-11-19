184e06378d290c8ed9b49899ba0cd5a5d39fc89f
jhondonavan <jhondonavan@gmail.com>
2015-11-19 15:48:05 -0200

    Time 21:18 aula 2

--- Aula 02 - PostGetModel/Aula 02 - PostGetModel/Content/HomeController.cs
+++ Aula 02 - PostGetModel/Aula 02 - PostGetModel/Content/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Aula_02___PostGetModel.Models;
 
 namespace Aula_02___PostGetModel.Content
 {
@@ -13,7 +14,16 @@ namespace Aula_02___PostGetModel.Content
 
         public ActionResult Index()
         {
-            return View();
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Jhon Donavan",
+                Email = "jhonDonavan@gmail.com"
+            };
+
+            
+
+            return View(pessoa);
         }
 
     }
