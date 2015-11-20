81472b75646b77b5c193a5de0b7a55416175a317
jhondonavan <jhondonavan@gmail.com>
2015-11-20 11:00:23 -0200

    Pausado TIME 39:04 aula 04

--- Aula 04 - RotasMVC/Aula 04 - RotasMVC/Controllers/HomeController.cs
+++ Aula 04 - RotasMVC/Aula 04 - RotasMVC/Controllers/HomeController.cs
@@ -1,20 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.EnterpriseServices;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Aula_04___RotasMVC.Models;
 
 namespace Aula_04___RotasMVC.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        
+        private readonly IEnumerable<Noticia> todasNoticias;
+
+        public HomeController()
+        {
+            todasNoticias = new Noticia().TodasAsNoticias().OrderByDescending(x => x.Data); 
+        }
 
         public ActionResult Index()
         {
-            return View();
+            var ultimasNoticias = todasNoticias.Take(3);
+            var todasCategorias = todasNoticias.Select(x => x.Categoria).Distinct().ToList();
+
+            ViewBag.Categorias = todasCategorias;
+;           return View(ultimasNoticias);
         }
 
+        public ActionResult TodasNoticias()
+        {
+            return View(todasNoticias);
+        }
+
+        public ActionResult MostrarNoticia(int noticiaId, string categoria, string titulo)
+        {
+            return View(todasNoticias.FirstOrDefault(x => x.NoticiaId == noticiaId));
+        }
+
+        public ActionResult MostraCategoria(string categoria)
+        {
+            var categoriaespecifica = todasNoticias.Where(x => x.Categoria == categoria);
+            ViewBag.Categoria = categoria;
+            return View(categoriaespecifica);
+
+        }
     }
 }
