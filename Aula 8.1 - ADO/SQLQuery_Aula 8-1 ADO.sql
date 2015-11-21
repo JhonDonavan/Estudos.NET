9f18fe79c4d4aced13073dc4e5688982dc91099d
jhondonavan <jhondonavan@gmail.com>
2015-11-21 14:08:55 -0200

    Fim aula 8.1 ADO introducao ao sql // somente script de banco criado

---
+++ Aula 8.1 - ADO/SQLQuery_Aula 8-1 ADO.sql
@@ -0,0 +1,11 @@
+select * from alunos
+
+insert into Alunos (Nome, Mae, DataNascimento) values ('Myeli', 'Ana Myeli', '10/05/1985')
+
+select * from alunos
+
+Update Alunos set Nome = 'Liane Myeli Casarin' where AlunoId = 3
+
+insert into Alunos (Nome, Mae, DataNascimento) values ('Flavia Lucia', 'lucia', '1991/04/17')
+
+Delete from Alunos where AlunoId = 3
