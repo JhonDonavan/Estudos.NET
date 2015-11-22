dd0177b9e6eaa7f1cadc5e469bd269f31538cd7e
jhondonavan <jhondonavan@gmail.com>
2015-11-22 09:44:19 -0200

    Aula 8.2 à 8.3

---
+++ Aula 8.3 Continuação TiSelvagem 8-2/Aula 8.3 ADO - Conexão.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Dos
+{
+    class Contexto:IDisposable
+    {
+
+        private readonly SqlConnection MinhaConexao;
+
+        public Contexto()
+        {
+            MinhaConexao = new SqlConnection(@"data source = NET-INFNET-V4\SQLEXPRESS; integrated security=SSPI; Initial Catalog=TiSelvagem");
+            MinhaConexao.Open();
+        }
+
+        public void ExecutaComando(string strQuery)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
